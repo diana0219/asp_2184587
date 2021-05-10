fbccdae20d7307bde2255f605bcb1f275ae2783f
diana0219 <dcsanabria71@misena.edu.co>
2021-05-10 18:34:46 -0500

    Agregar archivos de proyecto.

---
+++ asp_2184587/Controllers/ProvedorController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using asp_2184587.Models;
+
+namespace asp_2184587.Controllers
+{
+    public class ProvedorController : Controller
+    {
+
+    
+            public ActionResult Index()
+            {
+                using (var db = new inventarioEntities())
+                {
+                    return View(db.proveedor.ToList());
+                }
+            }
+
+            public ActionResult Create()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(proveedor proveedor)
+            {
+                if (!ModelState.IsValid)
+                    return View();
+
+                try
+                {
+                    using (var db = new inventarioEntities())
+                    {
+
+                        db.proveedor.Add(proveedor);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error " + ex);
+                    return View();
+                }
+            }
+
+
+
+            public ActionResult Edit(int id)
+            {
+                try
+                {
+                    using (var db = new inventarioEntities())
+                    {
+                        proveedor findUser = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                        return View(findUser);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error " + ex);
+                    return View();
+                }
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Edit(proveedor editUser)
+            {
+                try
+                {
+                    using (var db = new inventarioEntities())
+                    {
+                        proveedor user = db.proveedor.Find(editUser.id);
+
+                        user.nombre = editUser.nombre;
+                        user.direccion = editUser.direccion;
+                        user.telefono = editUser.telefono;
+                        user.nombre_contacto = editUser.nombre_contacto;
+
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error " + ex);
+                    return View();
+                }
+            }
+
+            public ActionResult Delete(int id)
+            {
+                try
+                {
+                    using (var db = new inventarioEntities())
+                    {
+                        var findUser = db.proveedor.Find(id);
+                        db.proveedor.Remove(findUser);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error " + ex);
+                    return View();
+                }
+            }
+
+        }
+    }
---
+++ asp_2184587/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_2184587.MvcApplication" Language="C#" %>
